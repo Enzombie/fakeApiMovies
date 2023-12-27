a0e1964a096eb12a21f7b6aef22ddd56519a1332
Enzo Lopez <enzofabianlopez96@gmail.com>
2023-12-26 23:23:38 -0300

    actualizando mongo

--- web/server-with-mongo.js
+++ web/server-with-mongo.js
@@ -2,5 +2,6 @@ import { createApp } from "../app.js";
 import { MovieModel} from "../models/mongoDB/movie.js";
 import mongoose from 'mongoose'
 
-mongoose.connect('mongodb+srv://Henso:miprimerapp@miapp.u8aovzq.mongodb.net/apiMovies?retryWrites=true&w=majority')
+const mongoUri = 'mongodb+srv://Henso:miprimerapp@miapp.u8aovzq.mongodb.net/apiMovies?retryWrites=true&w=majority'
+mongoose.connect(process.env.MONGO.CONNECT.URI)
 createApp({MovieModel: MovieModel});
