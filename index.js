17416f686fd4334552c0649a023dec819422a606
Enzo Lopez <enzofabianlopez96@gmail.com>
2023-12-28 01:07:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //este archivo es para correr correctamente la app en vercel junto a mongo
 import { createApp } from "./app.js";
-import { MovieModel} from "./models/mongoDB/movie.js";
+import { MovieModel} from "./models/mongo.js";
 import mongoose from 'mongoose'
 
 mongoose.connect(process.env.MONGO_CONNECT_URI)
