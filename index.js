100874bd60d2a16348852c3715a454b08376f40e
Enzo Lopez <enzofabianlopez96@gmail.com>
2023-12-28 02:13:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 //este archivo es para correr correctamente la app en vercel junto a mongo
-import {config} from 'dotenv'
 import { createApp } from "./app.js";
 import { MovieModel} from "./models/mongo.js";
 import mongoose from 'mongoose'
-config()
 
 ;( async () => {
     try {
-        await mongoose.connect(process.env.MONGO_CONNECT_URI)
+        await mongoose.connect(process.env.MONGODB_URI)
         console.log("Connected to mongoose successfully")
     } catch (error) {
         console.log("Connect failed: " + error.message)
